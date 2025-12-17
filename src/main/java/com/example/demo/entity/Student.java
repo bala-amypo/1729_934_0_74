505ca5a4a81cdc0fcad4d7f4bee4353f3182fafd
bala-amypo <bala@amypo.in>
2025-12-17 06:10:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,51 @@
+package com.example.demo.Entity;
+
+
+import java.time.LocalDate;
+
+import jakarta.annotation.Generated;
+import jakarta.xml.bind.annotation.XmlID;
+
+@Entity
+
+public class Student{
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private float cgpa;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+    public float getCgpa() {
+        return cgpa;
+    }
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    
+}
